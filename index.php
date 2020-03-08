c8121dd2dbe9f947d145e8d6384b833a0e4bd989
aryamaulana414 <61939670+aryamaulana414@users.noreply.github.com>
2020-03-08 20:55:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Web Karyawan</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+<body>
+<div class="top">
+    <div class='bghitam'></div>
+        <div class="menu">
+            <a href=""><img src="img/bg.png"></a>
+            <ul>
+                <li><a href="index.php">BERANDA</a></li>
+                <li><a href="absensi/data/index.php">DATA KARYAWAN</a></li>
+                <li><a href="absensi/index.php">ABSENSI KARYAWAN</a></li>
+                
+            </ul>
+        </div>
+        <div class="text">
+            <h1>WEB KARYAWAN<br>Dinas Perhubungan</h1>
+        </div>
+</div>
+
+     <div class="kontak">
+        <div class="des">
+        <img src="admin/img/dishub.png" width="212" height="136">
+            <h1>Pusat Informasi</h1>
+            <h2>KANTOR DINAS PERHUBUNGAN</h2>
+            <p>Jl. Banjaran, Tasikmalaya, Jawa Barat, Indonesia</p>
+            <h2>Kontak kami</h2>
+            <p>Jumat , 07.00 WIB - 16.00 WIB</p>
+       </div>
+    </div>
+    <div class="footer"><p>© 2020</a></p></div>
+</body>
+</html>
+
